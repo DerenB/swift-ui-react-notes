7a20090b4e011df2902ab25d5a84352036f8ef51
Deren B <47374311+DerenB@users.noreply.github.com>
2024-07-21 21:46:01 -0400

    Project Creation

---
+++ Demo-SwiftUI-MapApp/Demo-SwiftUI-MapApp/Demo_SwiftUI_MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_SwiftUI_MapAppApp.swift
+//  Demo-SwiftUI-MapApp
+//
+//  Created by Deren Bozer on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Demo_SwiftUI_MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
