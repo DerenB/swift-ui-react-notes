06f246a9e43b044351d4908d5f77c29f4d758cca
Deren B <47374311+DerenB@users.noreply.github.com>
2024-07-21 21:56:51 -0400

    Creating Location Model

---
+++ Demo-SwiftUI-MapApp/Demo-SwiftUI-MapApp/Models/Location.swift
@@ -0,0 +1,24 @@
+//
+//  Location.swift
+//  Demo-SwiftUI-MapApp
+//
+//  Created by Deren Bozer on 7/21/24.
+//
+
+
+import Foundation
+import MapKit
+
+
+struct Location {
+    
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+    
+}
+
