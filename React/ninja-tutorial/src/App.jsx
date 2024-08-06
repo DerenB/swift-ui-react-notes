6498298809282fd050ca890d627773e5b8367f5f
Deren B <47374311+DerenB@users.noreply.github.com>
2024-08-05 21:46:08 -0400

    React notes creation

---
+++ React/ninja-tutorial/src/App.jsx
@@ -0,0 +1,13 @@
+import '../styles/App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='content'>
+        <h1>App Component</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
